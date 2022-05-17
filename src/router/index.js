eabc8e6e76afcf49fa99ac34f0cc032744de6ee3
acsjiasuo <jiasuo91@gmail.com>
2022-05-17 10:57:48 +0200

    Changed to WebHistory from Hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from "vue-router"
+import {createRouter, createWebHistory} from "vue-router"
 
 // Components
 import Home from "../pages/Home"
@@ -42,7 +42,7 @@ const routes = [
 ]
 
 export default createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
     scrollBehavior(to, from, savedPosition) {
         // always scroll to top
